5a858e22b703f427fa5c2a6500e51ef7606fc3be
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-23 08:27:08 +0300

    Display registered dogs in the dom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/dogs')
+    .then(result => result.json())
+    .then(data => {
+        console.log(data)
+        const domDogsTable = document.getElementById('table-body')
+        domDogsTable.style.tableLayout = "auto"
 
+        for(dog of data){
+            const domDog = document.createElement('tr')
+            domDog.innerHTML = `
+            <td>${dog.name}</td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td style="display:flex; justify-content: center"><button>Edit</button></td>`
+
+            domDogsTable.append(domDog)
+        }
+        console.log(domDogsTable)
+
+    })
 })
